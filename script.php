5351e817ec3092ffe56d818af9e07e078b02372b
mttronc <mttronc@b68487ed-25af-4cb2-96ca-644de152e200>
2014-05-09 13:43:35 +0000

    move install/uninstall/update-scripts to script.php and sql-files to own folder
    git-svn-id: svn+ssh://svn.code.sf.net/p/commitglieder/code/trunk/com_mitglieder@78 b68487ed-25af-4cb2-96ca-644de152e200

---
+++ script.php
@@ -0,0 +1,62 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+class com_MitgliederInstallerScript
+{
+	function install($parent)
+	{
+	}
+
+	function uninstall($parent)
+	{
+		if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
+		require_once( JPATH_ADMINISTRATOR .DS. 'components' .DS. 'com_mitglieder' .DS. 'lib'.DS. 'config'.DS. 'config.php' );
+		require_once( JPATH_ADMINISTRATOR .DS. 'components' .DS. 'com_mitglieder' .DS. 'lib'.DS. 'upload'.DS. 'image.php' );
+
+		$db = JFactory::getDBO();
+		$config = Config::getConfig(array("delete_database","delete_pictures"));
+
+		/*
+		 * Löschen der Mitgliederbilder
+		 */
+		if($config['delete_pictures'] == "1") {
+			$query = "SELECT image_orginal, image_resize, image_thumb
+				FROM #__mitglieder_mitglieder";
+			$db->setQuery( $query );
+			$images = $db->loadObjectList();
+			Image::deleteImages($images);
+			echo '<p>Mitgliederbilder wurden gel&ouml;scht.</p>';
+		}
+		else echo '<p>Mitgliederbilder wurden nicht gel&ouml;scht.</p>';
+
+		/*
+		 * Löschen der Datenbank-Tabellen
+		 */
+		if($config['delete_database'] == "1") {
+			$query = "DROP TABLE IF EXISTS #__mitglieder_abteilungen, " . 
+				" #__mitglieder_felder, " .
+				" #__mitglieder_listen, " .
+				" #__mitglieder_mitglieder, " .
+				" #__mitglieder_mitglieder_felder, " .
+				" #__mitglieder_mitglieder_abteilungen," .
+				" #__mitglieder_abteilungen_felder," .
+				" #__mitglieder_config ";
+			$db->setQuery( $query );
+			$db->query();
+			echo '<p>Datenbankeintr&auml;ge wurden gel&ouml;scht.</p>';
+		}
+		else echo '<p>Datenbankeintr&auml;ge wurden nicht gel&ouml;scht.</p>';
+	}
+
+	function update($parent)
+	{
+	}
+
+	function preflight($type, $parent) 
+	{
+	}
+
+	function postflight($type, $parent) 
+	{
+	}
+}
