07b06647bfa26ed29a59a24e1889c2a935e70da0
mrtnmtth <mrtn.mtth@gmx.de>
2018-09-25 18:04:50 +0200

    Admin: Use delete() from JControllerAdmin

--- admin/controllers/abteilungen.php
+++ admin/controllers/abteilungen.php
@@ -4,26 +4,24 @@ defined('_JEXEC') or die();
 
 class MitgliederControllerAbteilungen extends JControllerAdmin
 {
-	function __construct()
-	{
-		parent::__construct();
+  function __construct()
+  {
+    parent::__construct();
+  }
 
-		$this->redirectPath = "index.php?option=com_mitglieder&view=abteilungen";
-	}
-
-	function delete()
-	{
-		$cid = $this->input->get('cid', array(), 'array');
-		$model = $this->getModel('abteilung');
-		if(!$model->delete($cid)) {
-			$msg = JText::_( 'Fehler: Abteilung(en) konnten nicht gelöscht werden' );
-		} else {
-			$msg = JText::_( 'Abteilung(en) Gelöscht' );
-		}
-
-		$cache = JFactory::getCache('com_mitglieder');
-		$cache->clean();
-
-		return $this->setRedirect($this->redirectPath, $msg);
-	}
+  /**
+   * Method to get a model object, loading it if required.
+   *
+   * @param   string  $name    The model name. Optional.
+   * @param   string  $prefix  The class prefix. Optional.
+   * @param   array   $config  Configuration array for model. Optional.
+   *
+   * @return  JModelLegacy  The model.
+   *
+   * @since   2.0
+   */
+  public function getModel($name = 'Abteilung', $prefix = 'MitgliederModel', $config = array('ignore_request' => true))
+  {
+    return parent::getModel($name, $prefix, $config);
+  }
 }
--- admin/controllers/mitglieder.php
+++ admin/controllers/mitglieder.php
@@ -4,26 +4,24 @@ defined('_JEXEC') or die();
 
 class MitgliederControllerMitglieder extends JControllerAdmin
 {
-	function __construct()
-	{
-		parent::__construct();
+  function __construct()
+  {
+    parent::__construct();
+  }
 
-		$this->redirectPath = "index.php?option=com_mitglieder&view=mitglieder";
-	}
-
-	function delete()
-	{
-		$cid = $this->input->get('cid', array(), 'array');
-		$model = $this->getModel('mitglied');
-		if(!$model->delete($cid)) {
-			$msg = JText::_( 'Fehler: Mitglied(er) konnten nicht gelöscht werden' );
-		} else {
-			$msg = JText::_( 'Mitglied(er) Gelöscht' );
-		}
-
-		$cache = JFactory::getCache('com_mitglieder');
-		$cache->clean();
-
-		return $this->setRedirect($this->redirectPath, $msg);
-	}
+  /**
+   * Method to get a model object, loading it if required.
+   *
+   * @param   string  $name    The model name. Optional.
+   * @param   string  $prefix  The class prefix. Optional.
+   * @param   array   $config  Configuration array for model. Optional.
+   *
+   * @return  JModelLegacy  The model.
+   *
+   * @since   2.0
+   */
+  public function getModel($name = 'Mitglied', $prefix = 'MitgliederModel', $config = array('ignore_request' => true))
+  {
+    return parent::getModel($name, $prefix, $config);
+  }
 }
